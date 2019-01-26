4791ca3f5b406f9344766b13cebdf0622ee3b810
ouafa1986 <46626891+ouafa1986@users.noreply.github.com>
2019-01-26 15:35:47 +0100

    Add files via upload

---
+++ JAVASP/java.js
@@ -0,0 +1,18 @@
+
+
+function Gras(){
+
+    document.getElementById("Saisir").style.fontWeight = "bold" ;
+
+ }
+
+ function Ital(){
+
+    document.getElementById("Saisir").style.fontStyle = "italic" ;
+
+ }
+ function Under(){
+
+    document.getElementById("Saisir").style.textDecoration = "underline" ;
+
+ }
